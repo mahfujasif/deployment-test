9b7707d11e0ffdc9d70ef923f55b71ab3deb12a7
Mahfuj <mahfuj@sekai-lab-bd.com>
2019-07-05 10:39:35 +0600

    DB removed

--- src/main/java/com/practise/users/UserControllers.java
+++ src/main/java/com/practise/users/UserControllers.java
@@ -1,12 +1,10 @@
 package com.practise.users;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,21 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class UserControllers {
   
-  private final UserRepository userRepository;
 
   @RequestMapping(value = "/user", method = RequestMethod.GET)
-  public ResponseEntity<List<User>> get() {
-    log.info("Fetching details of all users");
-    List<User> users = userRepository.findAll();
-    log.info("Fetched users: {}", users);
-    return new ResponseEntity<List<User>>(users, HttpStatus.OK);
-  }
-
-  @RequestMapping(value = "/user", method = RequestMethod.POST)
-  public ResponseEntity<?> create(@RequestBody User user) {
-    log.info("Creating user: {}", user);
-    userRepository.save(user);
-    return new ResponseEntity<>(HttpStatus.OK);
+  public ResponseEntity<User> get() {
+    log.info("Fetching details of all user");
+    User user = new User();
+    log.info("Fetched users: {}", user);
+    return new ResponseEntity<User>(user, HttpStatus.OK);
   }
 
 }
--- src/main/java/com/practise/users/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.practise.users;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-  
-  
-
-}
